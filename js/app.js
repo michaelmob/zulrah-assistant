d348be5fd797f59d5ef56755e2ad95f660a2f47c
thetarkus <michaelmob@protonmail.com>
2016-08-17 14:57:38 -0400

    Add shortcut keys

--- js/app.js
+++ js/app.js
@@ -1,30 +1,57 @@
-playerPositions = [ "bottom left", "left", "top", "right", "bottom right" ];
+playerPositions = ["bottom left", "left", "top", "right", "bottom right"];
 
 function $(selector) { return document.querySelector(selector); }
 function $$(selector) { return document.querySelectorAll(selector); }
 
-
 function hideAllRotations() {
 	var rotations = $$(".rotation,.selection");
 
-	for (var i = rotations.length - 1; i >= 0; i--) {
+	for (var i = rotations.length - 1; i >= 0; i--)
 		rotations[i].classList.add("hidden");
-	}
 
 	$(".pre-rotation").classList.add("hidden");
 	$(".second.phase").classList.add("hidden");
 }
 
-// Modify Stages to add Zulrah phases
-var stages = $$(".stage")
-for (var i = stages.length - 1; i >= 0; i--) {
-	var data = stages[i].getAttribute("data-phase").split("|");
+function restart() {
+	// Restart
+	hideAllRotations();
 
+	// Show starting phase and selection
+	$(".pre-rotation").classList.remove("hidden");
+	$(".first.selection").classList.remove("hidden");
+	$(".first.phase").classList.remove("hidden");
+
+	// Remove temporary faded class
+	var stages = $$(".temporary.faded");
+
+	for (var i = stages.length - 1; i >= 0; i--)
+		stages[i].classList.remove("temporary", "faded");
+}
+
+function fadeNext() {
+	var el = $(".rotation:not(.hidden) .stage:not(.faded)");
+
+	// Fade next
+	if (el != undefined)
+		el.classList.add("temporary", "faded");
+
+	// Restart
+	else
+		$(".restart").click();
+}
+
+/* Add phases to stages */
+var stages = $$(".stage");
+for (var i = stages.length - 1; i >= 0; i--) {
 	var player = document.createElement("span");
 	var zulrah = document.createElement("span");
 
-	player.className = "player " + playerPositions[data[1] - 1];
-	zulrah.className = "zulrah " + data[0];
+	var playerPosition = stages[i].getAttribute("data-player");
+	var zulrahPhase = stages[i].getAttribute("data-phase");
+
+	player.className = "player " + playerPositions[playerPosition - 1];
+	zulrah.className = "zulrah " + zulrahPhase;
 
 	stages[i].appendChild(player);
 	stages[i].appendChild(zulrah);
@@ -32,13 +59,13 @@ for (var i = stages.length - 1; i >= 0; i--) {
 	stages[i].removeAttribute("data");
 }
 
-// Click Listeners
-var stages = $$(".select .stage:not(.next)");
+/* Rotation selectors */
+var stages = $$(".select .stage:not([data-rotation='-'])");
 for (var i = stages.length - 1; i >= 0; i--) {
 	stages[i].addEventListener("click", function() {
 		var rotation = this.getAttribute("data-rotation");
 
-		if(rotation == undefined)
+		if (rotation == undefined)
 			return;
 
 		// Hide Rotations
@@ -49,8 +76,8 @@ for (var i = stages.length - 1; i >= 0; i--) {
 	});
 }
 
-// Show second selectino
-$(".stage.next").addEventListener("click", function() {
+/* Second selection */
+$(".stage[data-rotation='-']").addEventListener("click", function() {
 	// Hide first phase
 	$(".first.phase").classList.add("hidden");
 	$(".first.selection").classList.add("hidden");
@@ -60,29 +87,54 @@ $(".stage.next").addEventListener("click", function() {
 	$(".second.selection").classList.remove("hidden");
 });
 
-$(".restart").addEventListener("click", function() {
-	// Restart
-	hideAllRotations();
+/* Restart Button */
+$(".restart").addEventListener("click", restart);
 
-	// Show starting phase and selection
-	$(".pre-rotation").classList.remove("hidden");
-	$(".first.selection").classList.remove("hidden");
-	$(".first.phase").classList.remove("hidden");
-
-	// Remove temporary faded class
-	var stages = $$(".temp.faded");
-
-	for (var i = stages.length - 1; i >= 0; i--) {
-		stages[i].classList.remove("temp", "faded");
-	}
-});
-
-$(".current").addEventListener("click", function() {
-	var el = $(".rotation:not(.hidden) .stage:not(.faded)");
-	if(el != undefined)
-		el.classList.add("temp", "faded");
-});
-
+/* Instructions Button */
 $(".instructions").addEventListener("click", function() {
 	$("table.instructions").classList.toggle("hidden");
 });
+
+/* Next Fade */
+$(".current").addEventListener("click", fadeNext);
+
+/* Shortcut Keys */
+document.body.onkeyup = function(e) {
+	// Spacebar
+	if (e.keyCode == 32) {
+		fadeNext();
+	}
+
+	// R
+	else if (e.keyCode == 82) {
+		restart();
+	}
+
+	// 1
+	else if (e.keyCode == 49) {
+		if (!$(".first.selection").classList.contains("hidden"))
+			$(".first.selection [data-shortcut='1']").click();
+
+		else if (!$(".second.selection").classList.contains("hidden"))
+			$(".second.selection [data-shortcut='1']").click();
+	}
+
+	// 2
+	else if (e.keyCode == 50) {
+		if (!$(".first.selection").classList.contains("hidden"))
+			$(".first.selection [data-shortcut='2']").click();
+
+		else if (!$(".second.selection").classList.contains("hidden"))
+			$(".second.selection [data-shortcut='2']").click();
+	}
+
+	// 3
+	else if (e.keyCode == 51) {
+		if (!$(".first.selection").classList.contains("hidden"))
+			$(".first.selection [data-shortcut='3']").click();
+
+		else if (!$(".second.selection").classList.contains("hidden"))
+			$(".second.selection [data-shortcut='3']").click();
+	}
+
+}
